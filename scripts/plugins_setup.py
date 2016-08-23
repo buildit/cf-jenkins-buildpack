1a17989743c90ad7200e80649c76e1dded408a55
dermot <dermotmburke@gmail.com>
2016-08-23 10:12:50 +0100

    Remove scripts and war file - these will now live in common

--- scripts/plugins_setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.dir_util import copy_tree
-
-JENKINS_HOME = "/home/vcap/.jenkins/plugins"
-SOURCE = "plugins"
-
-def setup(source=SOURCE, destination=JENKINS_HOME):
-    copy_tree(source, destination)
